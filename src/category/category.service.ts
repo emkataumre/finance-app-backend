d4302bcc3dd028e737d57ca7b9ac8d456c9f2601
emkataumre <emo.vladinov@gmail.com>
2024-02-20 16:26:14 +0100

    fixed lintings again

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
